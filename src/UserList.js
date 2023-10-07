c4650ca0dd6f837e5a530915033308a87c2c7a59
Taysir655 <benaichataysir9@gmail.com>
2023-10-07 19:30:14 +0100

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import React ,  { useEffect, useState } from "react";
+import './App.css';
+
+
+const UserList = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    function getListUser() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+
+        .then(function (response) {
+          setData(response.data); // Stocker la réponse complète
+        })
+        .catch((err) => setError(err));
+    }
+    getListUser();
+  }, []);
+  console.log("", data);
+  return (
+    <div>
+      <div className="container mt-5">
+        <h1 className="text-center">Liste des Utilisateurs</h1>
+        <div className="row">
+          {data.map((user) => (
+            <div key={user.id} className="col-md-4 mb-3">
+              <p>{user.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default UserList;
